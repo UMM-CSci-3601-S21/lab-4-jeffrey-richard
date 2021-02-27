1175dded6e3727acac5a63fa06076e246885fe7b
Richard Lussier <lussi036@morris.umn.edu>
2021-02-27 16:50:37 -0600

    Implement todo.service and its tests
    Implemented the methods inside of todo.service and properly tested each method inside of the class.

--- client/src/app/todos/todo.service.ts
+++ client/src/app/todos/todo.service.ts
@@ -1,9 +1,54 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+import { Todo } from './todo';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
+  readonly todoUrl: string = environment.apiUrl + 'todo';
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  // Why is status not able to be passed as a boolean?
+  getTodos(filters?: { owner?: string; status?: string }): Observable<Todo[]> {
+    let httpParams: HttpParams = new HttpParams();
+    if(filters) {
+      if(filters.status) {
+        httpParams = httpParams.set('status', filters.status);
+      }
+      if(filters.owner) {
+        httpParams = httpParams.set('owner', filters.owner);
+      }
+    }
+    return this.httpClient.get<Todo[]>(this.todoUrl, {
+      params: httpParams,
+    });
+  }
+
+  getTodoById(id: string): Observable<Todo> {
+    return this.httpClient.get<Todo>(this.todoUrl + '/' + id);
+  }
+
+  filterTodos(todos: Todo[], filters: { category?: string; body?: string; limit?: number }): Todo[] {
+    let filteredTodos = todos;
+
+    if(filters.category) {
+      filters.category = filters.category.toLowerCase();
+
+      filteredTodos = filteredTodos.filter(todo => todo.category.toLowerCase().indexOf(filters.category) !== -1);
+    }
+    if(filters.body) {
+      filters.body = filters.body.toLowerCase();
+
+      filteredTodos = filteredTodos.filter(todo => todo.body.toLowerCase().indexOf(filters.body) !== -1);
+    }
+    if(filters.limit) {
+      filteredTodos = filteredTodos.slice(0,filters.limit);
+    }
+    return filteredTodos;
+  }
 }
