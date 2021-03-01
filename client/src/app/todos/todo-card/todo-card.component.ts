f06a22415b8062b3a8f6bcb666aa0ab3faeeecd1
Jeffrey Miller <Mill7707@umn.edu>
2021-03-01 15:49:42 -0600

    Add Todo cards to interface and modify their CSS files

--- client/src/app/todos/todo-card/todo-card.component.ts
+++ client/src/app/todos/todo-card/todo-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Todo } from '../todo';
 
 @Component({
   selector: 'app-todo-card',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo-card.component.scss']
 })
 export class TodoCardComponent implements OnInit {
+
+  @Input() todo: Todo;
+  @Input() simple ? = false;
 
   constructor() { }
 
